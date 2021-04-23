02a907164002aaa849a110cec44339a40a7938cc
Eugene Pyvovarov <eugene@lifeisgoodlabs.com>
2021-04-23 11:08:47 +0300

    added auto updates and comments during the script run

--- server-setup.sh
+++ server-setup.sh
@@ -26,6 +26,7 @@ OTHER_PUBLIC_KEYS_TO_ADD=(
 
 # Add sudo user and grant privileges
 useradd --create-home --shell "/bin/bash" --groups sudo "${USERNAME}"
+echo "[√] Added user '$USERNAME' to system and sudo group"
 
 # Check whether the root account has a real password set
 encrypted_root_pw="$(grep root /etc/shadow | cut --delimiter=: --fields=2)"
@@ -43,6 +44,7 @@ fi
 
 # Expire the sudo user's password immediately to force a change
 chage --lastday 0 "${USERNAME}"
+echo "[√] Set password for '$USERNAME' to immediately expire to force a change on first login"
 
 # Create SSH directory for sudo user
 home_directory="$(eval echo ~${USERNAME})"
@@ -52,6 +54,7 @@ mkdir --parents "${home_directory}/.ssh"
 if [ "${COPY_AUTHORIZED_KEYS_FROM_ROOT}" = true ]; then
     cp /root/.ssh/authorized_keys "${home_directory}/.ssh"
 fi
+echo "[√] Copied ssh-keys from root user to '$USERNAME' home folder"
 
 # Add additional provided public keys
 for pub_key in "${OTHER_PUBLIC_KEYS_TO_ADD[@]}"; do
@@ -68,3 +71,21 @@ sed --in-place 's/^PermitRootLogin.*/PermitRootLogin prohibit-password/g' /etc/s
 if sshd -t -q; then
     systemctl restart sshd
 fi
+echo "[√] Disabled toor SSH login with password"
+
+apt-get -y install unattended-upgrades && \
+cat > /etc/apt/apt.conf.d/10periodic <<EOF
+APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Download-Upgradeable-Packages "1";
+APT::Periodic::AutocleanInterval "7";
+APT::Periodic::Unattended-Upgrade "1";
+EOF
+sed -i 's#//      "o=Debian,n=jessie"#        "o=Debian,n=jessie"#' /etc/apt/apt.conf.d/50unattended-upgrades && \
+sed -i 's#//      "o=Debian,n=jessie-updates"#        "o=Debian,n=jessie-updates"#' /etc/apt/apt.conf.d/50unattended-upgrades && \
+sed -i 's#//\t"${distro_id}:${distro_codename}-updates"#\t"${distro_id}:${distro_codename}-updates"#' /etc/apt/apt.conf.d/50unattended-upgrades && \
+sed -i 's#//Unattended-Upgrade::Remove-Unused-Dependencies "false"#Unattended-Upgrade::Remove-Unused-Dependencies "true"#' /etc/apt/apt.conf.d/50unattended-upgrades && \
+sed -i '/codename=..distro_codename.-updates/ s#^//#  #' /etc/apt/apt.conf.d/50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades && \
+sed -i 's#//Unattended-Upgrade::Automatic-Reboot-Time "02:00"#Unattended-Upgrade::Automatic-Reboot-Time "04:00"#' /etc/apt/apt.conf.d/50unattended-upgrades
+DIST=$(lsb_release -c | cut -f 2); [ "${DIST}" != "jessie" ] && sed -i "s/jessie/${DIST}/g" /etc/apt/apt.conf.d/50unattended-upgrades
+
+echo "[√] Installed automatic daily updates of installed packages"
